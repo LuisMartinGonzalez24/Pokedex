5df7c5c3b20b65a2494fee6c14eef8c153ffe031
Luis Gonzalez Smarttec <luismartingt@gmail.com>
2021-10-29 10:07:59 -0400

    - Adding folder fonts and fonts file - Folder separte for static images

---
+++ app/hooks/useGetPokemonDetail.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+import { PokemonFullDetails } from "../interfaces/pokemonInterfaces";
+import { pokemonApi } from '../api/pokemonAPI';
+
+export const useGetPokemonDetail = (id: string) => {
+    
+
+    const [isLoading, setisLoading] = useState(true);
+    const [pokemonFullDetail, setpokemonFullDetail] = useState<PokemonFullDetails>({} as PokemonFullDetails);
+
+    const getPokemonFullDetails =  async () => {
+        const response = await pokemonApi.get<PokemonFullDetails>(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        setpokemonFullDetail(response.data);
+        setisLoading(false);
+    }
+
+    useEffect(() => {
+        getPokemonFullDetails();
+    }, [])
+
+    return {
+        isLoading,
+        pokemonFullDetail,
+    }
+}
